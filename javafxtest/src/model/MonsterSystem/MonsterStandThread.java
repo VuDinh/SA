4bc020b86f8ea3b31a7910f419536f364f86d2e1
longhero3 <longhero3@yahoo.com>
2013-04-03 14:49:42 +0700

    asdf

---
+++ javafxtest/src/model/MonsterSystem/MonsterStandThread.java
@@ -0,0 +1,33 @@
+package model.MonsterSystem;
+
+import View.Ingame.GameMap;
+import model.HeroSystem.Hero;
+import model.HeroSystem.HeroStatus;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: USER
+ * Date: 4/3/13
+ * Time: 1:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MonsterStandThread extends  Thread {
+    Monster monster;
+    GameMap panel;
+    public MonsterStandThread(Monster monster,GameMap panel){
+        this.monster = monster;
+        this.panel = panel;
+    }
+    @Override
+    public void run(){
+        while(true){
+            monster.nextSprite();
+            panel.repaint();
+            try {
+                Thread.sleep(80);
+            } catch (InterruptedException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+    }
+}
