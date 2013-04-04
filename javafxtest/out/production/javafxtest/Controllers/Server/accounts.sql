433b48bc9026083a2f21fdf07b4f73bb71b2e320
VuDinh <blackratxii@gmail.com>
2013-04-04 15:44:22 +0700

    asdfas

--- javafxtest/out/production/javafxtest/Controllers/Server/accounts.sql
+++ javafxtest/out/production/javafxtest/Controllers/Server/accounts.sql
@@ -1,9 +1,9 @@
 drop table if exists accounts;
 create table accounts(
   username text primary key,
-  message text
+  password text
 );
-insert into accounts(username,message) values('long','123');
-insert into accounts(username,message) values('dinh','123');
-insert into accounts(username,message) values('hung','123');
-insert into accounts(username,message) values('quan','123');
+insert into accounts(username,password) values('long','123');
+insert into accounts(username,password) values('dinh','123');
+insert into accounts(username,password) values('hung','123');
+insert into accounts(username,password) values('quan','123');
