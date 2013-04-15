c90bbe19953d1866cb3c985f856c37597d97cc15
VuDinh <blackratxii@gmail.com>
2013-04-16 00:03:25 +0700

    asdfasdf

---
+++ javafxtest/src/Controllers/Requests/PlayingGameRequest.java
@@ -0,0 +1,42 @@
+package Controllers.Requests;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 4/14/13
+ * Time: 9:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlayingGameRequest implements Serializable {
+    private int heroIndex,heroSlot,matchIndex;
+
+    public PlayingGameRequest(){
+
+    }
+
+    public int getHeroIndex() {
+        return heroIndex;
+    }
+
+    public void setHeroIndex(int heroIndex) {
+        this.heroIndex = heroIndex;
+    }
+
+    public int getHeroSlot() {
+        return heroSlot;
+    }
+
+    public void setHeroSlot(int heroSlot) {
+        this.heroSlot = heroSlot;
+    }
+
+    public int getMatchIndex() {
+        return matchIndex;
+    }
+
+    public void setMatchIndex(int matchIndex) {
+        this.matchIndex = matchIndex;
+    }
+}
