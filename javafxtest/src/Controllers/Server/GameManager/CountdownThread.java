91d139cace68babfa6aea50ca2a80973cab2f21f
longhero3 <longhero3@yahoo.com>
2013-04-16 13:25:15 +0700

    asfgxcb

---
+++ javafxtest/src/Controllers/Server/GameManager/CountdownThread.java
@@ -0,0 +1,12 @@
+package Controllers.Server.GameManager;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: long
+ * Date: 4/16/13
+ * Time: 1:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CountdownThread extends Thread {
+
+}
