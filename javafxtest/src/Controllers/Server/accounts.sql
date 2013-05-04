db79a16a46fdeba625f7069e96e3e3559378bb31
longhero3 <longhero3@yahoo.com>
2013-05-04 13:54:47 +0700

    asdf

--- javafxtest/src/Controllers/Server/accounts.sql
+++ javafxtest/src/Controllers/Server/accounts.sql
@@ -30,6 +30,7 @@ create table monsterPositions(
     row integer,
     col integer
 );
+--insert into monsterPositions(id,row,col) values(0,13,8);
 insert into monsterPositions(id,row,col) values(0,13,8);
 insert into monsterPositions(id,row,col) values(1,9,14);
 insert into monsterPositions(id,row,col) values(2,19,14);
